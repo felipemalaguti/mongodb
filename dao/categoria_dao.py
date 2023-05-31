32f1fb23619c02174c1a2e9c2ecd693b9340563c
Felipe Vido Malaguti <felipe.malaguti@eldorado.org.br>
2023-05-30 21:26:13 -0300

    Criando client e colocando categorias no banco de dados

--- dao/categoria_dao.py
+++ dao/categoria_dao.py
@@ -1,5 +1,6 @@
 from model.categoria import Categoria
 from database.client_factory import ClientFactory
+from bson import ObjectId
 
 class CategoriaDAO:
 
@@ -26,29 +27,21 @@ class CategoriaDAO:
         db.categorias.insert_one({'nome': categoria.nome})
         client.close()   
 
-    def remover(self, categoria_id: int) -> bool:
-        encontrado = False
-        for c in self.__categorias:
-            if (c.id == categoria_id):
-                index = self.__categorias.index(c)
-                self.__categorias.pop(index)
-                encontrado = True
-                break
-        return encontrado
+    def remover(self, categoria_id: str) -> bool:
+        client = self.__client_factory.get_client()
+        db = client.livraria
+        resultado = db.categorias.delete_one({'_id': ObjectId(categoria_id)})
+        if (resultado.deleted_count == 1):
+            return True
+        return False
 
-    def buscar_por_id(self, categoria_id) -> Categoria:
+    def buscar_por_id(self, categoria_id: str) -> Categoria:
         cat = None
-        for c in self.__categorias:
-            if (c.id == categoria_id):
-                cat = c
-                break
+        client = self.__client_factory.get_client()
+        db = client.livraria
+        resultado = db.categorias.find_one({'_id': ObjectId(categoria_id)})
+        if (resultado):
+            cat = Categoria(resultado['nome'])
+            cat.id = resultado['_id']
+        client.close()
         return cat
-    
-    def ultimo_id(self) -> int:
-        index = len(self.__categorias) -1
-        if (index == -1):
-            id = 0
-        else:
-            id = self.__categorias[index].id
-        return id
-    
